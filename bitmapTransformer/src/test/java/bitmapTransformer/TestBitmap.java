3a527e492f7370530152f9ca15c111425b9c2cb2
Matt Burger <matt.burger1@outlook.com>
2019-05-31 13:48:51 -0700

    tried using bmp from class, errors still exist for write method

--- bitmapTransformer/src/test/java/bitmapTransformer/TestBitmap.java
+++ bitmapTransformer/src/test/java/bitmapTransformer/TestBitmap.java
@@ -7,13 +7,13 @@ public class TestBitmap {
     //test read function
     @Test
     public void testAppReadsFile_success(){
-        Bitmap classUnderTest = new Bitmap("src/main/resources/input.bmp", "src/main/resources/", "output.bmp");
+        Bitmap classUnderTest = new Bitmap("src/main/resources/input2.bmp", "src/main/resources/", "output.bmp");
         assertEquals("getBitmap should return Success", "Success!", classUnderTest.getBitmap());
     }
 
     @Test
     public void testAppReadsFile_fail(){
-        Bitmap classUnderTest = new Bitmap("src/src/main/resources/input.bmp", "src/main/resources/", "output.bmp");
+        Bitmap classUnderTest = new Bitmap("src/src/main/resources/input2.bmp", "src/main/resources/", "output.bmp");
         //change to assert failure later
         assertEquals("getBitmap should return failure message", "Failed to read", classUnderTest.getBitmap());
     }
@@ -21,13 +21,13 @@ public class TestBitmap {
     //test write function
     @Test
     public void testAppWritesFile_success(){
-        Bitmap classUnderTest = new Bitmap("src/main/resources/input.bmp", "src/main/resources/output.bmp", "output");
+        Bitmap classUnderTest = new Bitmap("src/main/resources/input2.bmp", "src/main/resources/output.bmp", "output");
         assertEquals("writeToFile should return Success", "Success!", classUnderTest.writeToFile());
     }
 
     @Test
     public void testAppWritesFile_fail(){
-        Bitmap classUnderTest = new Bitmap("src/src/main/resources/input.bmp", "src/main/main/resources/output.bmp", "output");
+        Bitmap classUnderTest = new Bitmap("src/src/main/resources/input2.bmp", "src/main/main/resources/output.bmp", "output");
         //change to assert failure later
         assertEquals("writeToFile should return failure message", "Failed to write", classUnderTest.writeToFile());
     }
