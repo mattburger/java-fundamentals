1e44aab586d7888b72a3d6bca240713cf55b9cfd
Matt Burger <matt.burger1@outlook.com>
2019-06-05 09:49:38 -0700

    Added test for printing constructor. Test passed.

--- basiclibrary/src/test/java/inheritance/TestShop.java
+++ basiclibrary/src/test/java/inheritance/TestShop.java
@@ -10,6 +10,9 @@ public class TestShop {
     public void testShopConstructor(){
         Shop shop1 = new Shop("Da Shop", "This shop has provided premium service " +
                 "in the local area for the last 300 years.", 3);
+        String expectedOutputConstructor = "Da Shop: This shop has provided premium service " +
+                                "in the local area for the last 300 years.";
+        String actualOuputConstructor = shop1.toString();
 
         assertEquals("both names should be the same", "Da Shop", shop1.getName());
         assertEquals("both descriptions should be the same",
@@ -17,5 +20,6 @@ public class TestShop {
                         "in the local area for the last 300 years.",
                 shop1.getDescription() );
         assertEquals("both dollarSigns should be equal", 3, shop1.getDollarSigns() );
+        assertEquals("both string values should be equal", expectedOutputConstructor, actualOuputConstructor);
     }
 }
